02c8f30d40da34c133da25cccdef239238b193ac
Lucas Zanotti <ldvzanotti@gmail.com>
2021-03-20 21:22:42 -0300

    Minor changes

--- src/main/java/logging/Log.java
+++ src/main/java/logging/Log.java
@@ -8,22 +8,23 @@ public class Log {
 	public final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
 	public static void setupLogger() {
-
+		//Resets log configuration
 		LogManager.getLogManager().reset();
 		LOGGER.setLevel(Level.ALL);
-
+		//Console handler configuration
 		ConsoleHandler ch = new ConsoleHandler();
 		ch.setFormatter(new SimpleFormatter());
 		ch.setLevel(Level.INFO);
 		LOGGER.addHandler(ch);
-
+		//File handler configuration
 		try {
 			FileHandler fh = new FileHandler("Log.txt", true);
 			fh.setFormatter(new SimpleFormatter());
 			fh.setLevel(Level.FINE);
 			LOGGER.addHandler(fh);
-		} catch (
-				IOException e) {
+		}
+		//Throws exception if file handler is not working
+		catch (IOException e) {
 			LOGGER.log(Level.SEVERE, "Logger not working!", e);
 		}
 	}
